b0c7de3e73bda05b217f77602c162f3983adf2c4
Cindy Kei <myk245@nyu.edu>
2019-11-25 01:08:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,9 @@ def find_item_by_name_in_collection(name, collection)
   index = 0
   while index < collection.length do
     return collection[index] if name == collection[index][:item]
+    index += 1
+  end
+  nil
   # Implement me first!
   #
   # Consult README for inputs and outputs
